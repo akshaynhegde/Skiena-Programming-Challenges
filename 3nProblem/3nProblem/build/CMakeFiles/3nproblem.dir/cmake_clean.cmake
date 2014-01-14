ff763cd5d01b273e98983bfb48a06320161c2f7a
akshaynhegde <akshaynhegde@gmail.com>
2014-01-14 20:48:58 +0530

    initial commit with "hello world template"

---
+++ 3nProblem/3nProblem/build/CMakeFiles/3nproblem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/3nproblem.dir/main.o"
+  "3nproblem.pdb"
+  "3nproblem"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/3nproblem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
