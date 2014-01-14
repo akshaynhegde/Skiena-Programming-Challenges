fd97db3235506bb93e04aaed1c1b1192ab3bf38f
akshaynhegde <akshaynhegde@gmail.com>
2014-01-14 22:35:41 +0530

    3n+1 problem solution. shows the right output but for some reason its failing on the programming challenges site

--- 3nProblem/3nProblem/main.cpp
+++ 3nProblem/3nProblem/main.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
+#include <stdlib.h>
+using namespace std;
 
-int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+bool isEven(int number);
+int iterationsToreachOne(int number);
+
+int main(int argc, char **argv) {   
+    int x, y;
+    
+    if (argc > 2) {
+      x = atoi(argv[1]);
+      y = atoi(argv[2]);
+    }
+    
+    int mostIterations = 0;
+    for (int i=x;i <= y;i++) {
+      int iterations = iterationsToreachOne(i);
+    if (iterations > mostIterations)
+      mostIterations = iterations;
+  }
+  
+  cout << x << " " << y << " " <<  mostIterations;
+  return 0;
+}
+
+int iterationsToreachOne(int number) {
+     int count = 0;
+      while (number != 1) {
+	if (isEven(number)) {
+	  //do n/2
+	  number = number/2;
+	}
+      else {
+	//do 3n+1
+	number = 3*number+1;
+      }
+      count++;
+    }
+    return count+1;
+}
+
+bool isEven(int number) {
+  if (!(number % 2))
+    return true;
+  else 
+    return false;
 }
