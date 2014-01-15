99f09b401eb12e110eae90a361b42ff4c1daf17a
akshaynhegde <akshaynhegde@gmail.com>
2014-01-15 23:07:56 +0530

    intermediate commit. rows and columns might have been interchanged. too much duplicate code, long main function , looks like the shittiest code that can ever be written..:(

---
+++ mineSweeper/build/CMakeFiles/minesweeper.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/minesweeper.dir/main.o"
+  "minesweeper.pdb"
+  "minesweeper"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/minesweeper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- mineSweeper/main.cpp
+++ mineSweeper/main.cpp
@@ -1,6 +1,122 @@
 #include <iostream>
+using namespace std;
 
-int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+enum squareType { aMine=-1, notAMine=0 };
+
+int main() {
+    
+  //get the number of line and columns
+  int rows, columns;
+  cin >> rows >> columns;
+  cin.ignore(256,'\n');//ignore all characters until the next new line.
+
+  if (rows > 0 && columns > 0) {
+    //construct the 2D array from the field
+    int field[rows][columns];
+    
+    string line;
+    
+    int i=0,j=0;
+    for(i = 0; i < rows; i++) {
+      getline(cin,line);
+      for(j = 0; j < line.length(); j++) {
+	//read chars in the line
+	if (j == columns) break;
+	char aChar = line[j];
+	if(aChar == '*') {
+	  //a mine
+	  field[i][j] = aMine;
+	}
+	else if(aChar == '.') {
+	  //not a mine
+	  field[i][j] = notAMine;
+	}
+      }
+    }
+    
+	//iterate through the array
+      char mineField[rows][columns];
+      
+      for(int i=0 ; i<rows; i++) {
+	for(int j=0 ; j< columns ;j++) {
+	  cout<<field[i][j]<<endl;
+	    if(field[i][j] == aMine) {
+	      //update all non mine square that affect
+	      int x = i;
+	      int y = j;
+	      
+	      x = i-1;
+	      y = j-1;
+	      if(x >= 0 && y >= 0 && x < rows && y < columns) {
+		    if(field[x][y] != aMine){
+			field[x][y] += 1;
+		  }
+	      }
+
+	      x = i-1;
+	      y = j;
+	      if(x >= 0 && y >= 0 && x < rows && y < columns) {
+		    if(field[x][y] != aMine){
+			field[x][y] += 1;
+		  }
+	      }
+	      
+	      x = i-1;
+	      y = j+1;
+	      if(x >= 0 && y >= 0 && x < rows && y < columns) {
+		    if(field[x][y] != aMine){
+			field[x][y] += 1;
+		  }
+	      }
+	      
+	      x = i;
+	      y = j-1;
+	      if(x >= 0 && y >= 0 && x < rows && y < columns) {
+		    if(field[x][y] != aMine){
+			field[x][y] += 1;
+		  }
+	      }
+	      
+	      x = i;
+	      y = j+1;
+	      if(x >= 0 && y >= 0 && x < rows && y < columns) {
+		    if(field[x][y] != aMine){
+			field[x][y] += 1;
+		  }
+	      }
+	      
+	      x = i+1;
+	      y = j-1;
+	      if(x >= 0 && y >= 0 && x < rows && y < columns) {
+		    if(field[x][y] != aMine){
+			field[x][y] += 1;
+		  }
+	      }
+	      
+	      x = i+1;
+	      y = j;
+	      if(x >= 0 && y >= 0 && x < rows && y < columns) {
+		    if(field[x][y] != aMine){
+			field[x][y] += 1;
+		  }
+	      }
+	      
+	      x = i+1;
+	      y = j+1;
+	      if(x >= 0 && y >= 0 && x < rows && y < columns) {
+		    if(field[x][y] != aMine){
+			field[x][y] += 1;
+		  }
+	      }
+	    }
+	 }
+      }
+      
+       for(int i=0 ; i<rows; i++) {
+	for(int j=0 ; j< columns ;j++) {
+	  cout << field[i][j]<<endl;
+	}
+     }
+    }
+  return 0;
 }
